0786689f7b96bff59909f0e51303920343570cd1
ripplek <ripple_k@163.com>
2019-05-05 16:54:50 +0800

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by 张坤 on 2019/5/5.
+//  Copyright © 2019 ripple_k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
